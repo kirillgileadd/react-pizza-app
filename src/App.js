88ffe72ac8b1fd3759c77c90d8241e4b7a1f53a4
kirillgileadd <kirillgilead@gmail.com>
2021-09-06 14:58:11 +0300

    v.0.0.0.2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
             <div className="content">
                 <div className="container">
                     <div className="content__top">
-                        <Categories items={['Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']}/>
+                        <Categories items={['Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Открытые']}/>
                         <div className="sort">
                             <div className="sort__label">
                                 <svg
